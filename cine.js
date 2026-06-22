c1c0ff6a0e974f82b0ffec48bcf37754027d47fc
pedroamarop <pedro.a.p.dev@gmail.com>
2026-06-22 11:56:42 -0300

    dia 3

--- cine.js
+++ cine.js
@@ -1,15 +1,15 @@
-const nomeFilme = "Espetacular Homem Aranha";
-const dataLancamento = 2014;
-const classificacaoIndicativa = 12;
-const duracaoFilme = "2h 22min";
-let avaliacao = "6,6";
-const descricao = "Preocupado com o fantasma da promessa feita ao pai de Gwen Stacy, de que se afastaria para protegê-la dos vilões do Homem-Aranha, Peter Parker ainda deve lidar com o retorno do velho amigo Harry Osborn e o surgimento do poderoso Electro.";
-const diretor = "Marc Webb";
+const button = document.getElementById('submit')
 
-console.log(nomeFilme);
-console.log(dataLancamento);
-console.log(classificacaoIndicativa);
-console.log(duracaoFilme);
-console.log(avaliacao);
-console.log(descricao);
-console.log(diretor);
+button.addEventListener('click', () =>{
+    let nota = Number(document.getElementById('valor').value);
+
+    if (isNaN(nota) || nota > 10 || nota < 0){
+        console.log("digite uma nota válida")
+    }else if(nota > 8){
+        console.log("Sua nota é excelente!");
+    } else if(nota > 6){
+        console.log("Sua nota é boa!");
+    } else {
+        console.log("Regular")
+    }
+})
